30e601569240ee28dc76bccf9341c4548513f8bc
Peteris Caurs <peteris.dev@gmail.com>
2024-05-22 19:52:00 +0300

    implement device list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react'
+import DeviceList from './DeviceList'
 import Header from './Header'
 import Toolbar from './Toolbar'
+import axios from 'axios'
+import { Device } from './api'
 
 function App() {
+  const [deviceList, setDeviceList] = useState<Device[]>([])
+
+  useEffect(() => {
+    axios
+      .get('https://static.ui.com/fingerprint/ui/public.json')
+      .then((response) => setDeviceList(response.data.devices))
+  }, [])
+
   return (
     <>
       <Header author="Pēteris Čaurs" />
       <Toolbar />
+      <DeviceList devices={deviceList} />
     </>
   )
 }
---
+++ src/DeviceList.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+import { Device } from './api'
+
+const DeviceListContainer = styled.div`
+  margin: 23px 104px;
+`
+
+const StyledTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  table-layout: fixed;
+`
+
+const TableCell = styled.td`
+  padding: 5px;
+  border-bottom: 1px solid rgb(237, 237, 240);
+`
+
+const TableHeadCell = styled(TableCell)`
+  color: rgb(79, 79, 79);
+  font-weight: bold;
+  font-size: 14px;
+`
+
+const IconCell = styled(TableCell)`
+  display: flex;
+  justify-content: end;
+  padding-right: 24px;
+`
+
+interface DeviceListProps {
+  devices: Device[]
+}
+
+export default function DeviceList({ devices }: DeviceListProps) {
+  return (
+    <DeviceListContainer>
+      <StyledTable>
+        <thead>
+          <tr>
+            <IconCell style={{ color: '#BDBDBD' }} colSpan={1}>
+              {devices.length} devices
+            </IconCell>
+            <TableHeadCell colSpan={2}>PRODUCT LINE</TableHeadCell>
+            <TableHeadCell colSpan={5}>NAME</TableHeadCell>
+          </tr>
+        </thead>
+        <tbody>
+          {devices.map((device) => (
+            <tr>
+              <IconCell colSpan={1}>
+                <img
+                  src={`https://static.ui.com/fingerprint/ui/icons/${device.icon.id}_${device.icon.resolutions[1][0]}x${device.icon.resolutions[1][1]}.png`}
+                  alt={`${device.product.name}`}
+                />
+              </IconCell>
+              <TableCell colSpan={2}>{device.line.name}</TableCell>
+              <TableCell colSpan={5}>{device.product.name}</TableCell>
+            </tr>
+          ))}
+        </tbody>
+      </StyledTable>
+    </DeviceListContainer>
+  )
+}
